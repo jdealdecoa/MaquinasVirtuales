f07f314e720858f1b71b39303df22d808070e3d8
Maquina Virtual Clase <jimena.sanchez@enti.cat>
2024-11-27 17:36:34 +0100

    Server erady to run with Hello World

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log("Hello world")
+//Init app 
+//Cargo la clase del archivo
+const express = require("express");
+
+//Llamo al constructor
+const app = express();
+
+//seccion de variables de configuracion 
+app.set("port", process.PORT || 3000); //a la variable port, voy a que coja el puerto del proceso actual, si tenemos un servidor hecho pillará el puerto, pero si estamos en local, cogerá el 3000. 
+app.set ("json spaces", 2); //configuramos el comportamiento
+
+//Middlewares
+const morgan = require ("morgan"); 
+
+app.use(morgan("dev"));
+//app.use(morgan("combined"));
+
+//configuracion adicional de express
+app.use(express.urlencoded({extended: false}));
+app.use(express.json()); //si no le decimos a express que puede usar json, no podrá recibir y enviar json
+
+//clases auxiliares
+const ipHelper = require("ip");
+const qrcode = require("qrcode-terminal"); //crea codigo qr con la web q acabamos de hacer
+
+app.get("/", (req, res) => {  //toda la url
+
+    res.send("Hello World"); 
+
+});
+
+app.listen(app.get("port"), () => {
+
+    const ip = ipHelper.address();
+    const port = app.get("port"); 
+
+    const url = "https://" + ip + ":" + port + "/";
+
+    console.log("servidor arrancado en la url: " + url); 
+
+    qrcode.generate(url, {small: true},
+    (qrcode) => {
+        console.log(qrcode);
+    });
+});
