fb55067736a30ed5dc6b818505be242ded5a3790
Maquina Virtual Clase <jimena.sanchez@enti.cat>
2024-12-17 18:21:46 +0100

    separar sistema de sockets en salas

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -9,7 +9,21 @@ router.get("/", (req,res) => {
 
 const io = app.get("io");
 
+var rooms = [
+    { 
+        name : "Sala de pruebas 1", 
+        usersCount : 5,
+        message: ["Hola", "Adios"]
+    },
+    { 
+        name : "Sala de pruebas 2", 
+        usersCount : 7,
+        message: ["Hola2", "Adios2"]
+    }
+];
+
 var messages = [];
+
 
 io.on("connection", (socket) => {
 
@@ -17,8 +31,32 @@ io.on("connection", (socket) => {
     console.log ("socket Connected with IP:Port -->"
         + adress.remoteAdress + ":" + adress.remotePort);
 
+    //ROOM SELECTOR
+
+    socket.on("GetInitialRooms", ()=>{
+        socket.emit("ÍnitialRooms", rooms);
+    });
+
+    socket.on("EnterRoomClientToServer", (roomName) => {
+        for(room of rooms)
+        {
+            if(room.name === roomName)
+            {
+                room.usersCount +=1;     
+                socket.emit("EnterRoomServerToClient", room);
+                io.emit("RoomsUpdate", room);
+                break;
+            }
+        }
+    });
+
+    //CHAT CODE
+
+
+
     socket.on("ChatMessageClientToServer", (message) => {
 
+        
         //Socket emit ,envia el evento a un solo socket en concreto 
         // socket.emit("ChatMessageServerToClient", message);
 
